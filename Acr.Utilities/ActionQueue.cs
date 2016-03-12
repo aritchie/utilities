dbeca3e17b8d32fe68a165d1856f31a7a9aeedbe
Allan Ritchie <allan.ritchie@gmail.com>
2016-03-12 13:15:55 -0500

    Initial commit

---
+++ Acr.Utilities/ActionQueue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Acr.Utilities
+{
+    public class ActionQueue
+    {
+        readonly Queue<Action> actions = new Queue<Action>(); 
+    }
+}
---
+++ Acr.Utilities/TaskQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Acr.Utilities
+{
+    public class TaskQueue
+    {
+        public int MaxExecutions { get; set; } = 4;
+        readonly Queue<Task> tasks = new Queue<Task>();
+
+
+        public void Add(Task task)
+        {
+            this.tasks.Enqueue(task);
+        }
+
+
+        public void Start()
+        {
+            
+        }
+
+
+        public void Stop()
+        {
+            
+        }
+
+
+        public void CancelAll()
+        {
+            
+        }
+    }
+}
