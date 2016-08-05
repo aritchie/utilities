9b19e1e9ddbd6db84ef84d0d40f7c3f20856037c
Allan Ritchie <allan.ritchie@gmail.com>
2016-08-05 11:47:30 -0400

    moving geolocator extensions here

---
+++ Acr.Utilities/RxExtensions.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Reactive.Linq;
+
+
+//namespace Acr.Utilities
+//{
+//    public static class RxExtensions
+//    {
+//        public static IObservable<IList<T>> SlidingBuffer<T>(this IObservable<T> obs, TimeSpan span, int max)
+//        {
+//            return Observable.Create<IList<T>>(ob =>
+//            {
+//                var acc = new List<T>();
+//                return obs
+//                    .Buffer(span)
+//                    .Subscribe(next =>
+//                    {
+//                        if (next.Count == 0) //no activity in time span
+//                        {
+//                            ob.OnNext(acc);
+//                            acc.Clear();
+//                        }
+//                        else
+//                        {
+//                            acc.AddRange(next);
+//                            if (acc.Count >= max) //max items collected
+//                            {
+//                                ob.OnNext(acc);
+//                                acc.Clear();
+//                            }
+//                        }
+//                    },
+//                    ob.OnError,
+//                    () =>
+//                    {
+//                        ob.OnNext(acc);
+//                        ob.OnCompleted();
+//                    }
+//                );
+//            });
+//        }
+//    }
+//}
---
+++ Xamarin.Plugins.Geolocator.Extensions/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reactive.Linq;
+using Acr.Utilities;
+using GeoCoordinatePortable;
+using Plugin.Geolocator.Abstractions;
+
+
+namespace Xamarin.Plugins.Geolocator.Extensions
+{
+    public static class Extensions
+    {
+        public static IObservable<Distance> DerivedSpeed(this IGeolocator locator)
+        {
+            return Observable.Create<Distance>(ob =>
+            {
+                ob.OnNext(Distance.Empty);
+                GeoCoordinate lastPosition = null;
+
+                Observable
+                    .Interval(TimeSpan.FromSeconds(1))
+                    .Subscribe(x =>
+                    {
+                        // broadcast zero if new coordinate hasn't come in?
+                        //if (lastPosition == null)
+                    });
+                var changed = Observable
+                    .FromEventPattern<PositionEventArgs>(locator, "PositionChanged")
+                    .Throttle(TimeSpan.FromSeconds(1))
+                    .Select(x => x.EventArgs.Position)
+                    .Subscribe(x =>
+                    {
+                        var current = new GeoCoordinate(x.Latitude, x.Longitude);
+                        if (lastPosition == null)
+                        {
+                            lastPosition = current;
+                            return;
+                        }
+                        var distKm = Convert.ToDouble(lastPosition.GetDistanceTo(current));
+                        var dist = Distance.FromKilometers(distKm); // distance over the last second
+                        lastPosition = current;
+
+                        ob.OnNext(dist);
+                    });
+
+                locator.DesiredAccuracy = 200;
+                if (!locator.IsListening)
+                    locator.StartListeningAsync(1000, 200, false);
+
+                return changed;
+            });
+        }
+
+
+        public static IObservable<Distance> DerivedOdometer(this IGeolocator locator, int startingOdo = 0)
+        {
+            return Observable.Create<Distance>(ob =>
+            {
+                locator.DesiredAccuracy = 200;
+                if (!locator.IsListening)
+                    locator.StartListeningAsync(1000, 200, false);
+
+                return () => { };
+            });
+        }
+    }
+}
